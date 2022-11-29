0966311222062a9b144c0e338426be56f62039ad
안나광 <ahnnagwang@naver.com>
2022-11-29 13:53:28 +0900

    웹퍼블리싱 연습
    웹퍼블리싱 연습

---
+++ 34/javascript.js
@@ -0,0 +1,10 @@
+$(function () {
+  $('.btn li').click(function() {
+    $(this).addClass('active')
+    $(this).siblings().removeClass('active')
+
+    let result = $(this).attr('data-alt')
+    $('.tabs div').removeClass('active')
+    $('#' + result).addClass('active')
+  })
+})
