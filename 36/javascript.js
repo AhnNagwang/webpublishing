a9fe9371089649046f927b32e2de20dd0b091d36
안나광 <ahnnagwang@naver.com>
2022-12-01 19:12:57 +0900

    웹퍼블리싱 연습
    웹퍼블리싱 연습

---
+++ 36/javascript.js
@@ -0,0 +1,10 @@
+$(function () {
+  $('.size span, .color span').click(function () {
+    $(this).addClass('active')
+    $(this).siblings().removeClass('active')
+  })
+
+  $('.like').click(function () {
+    $(this).toggleClass('active')
+  })
+})
